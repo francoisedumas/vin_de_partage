fdec7be9c47a221a3af775780a7ec6d59f23d9ac
Francois Dumas <dumaslattaque.francois@gmail.com>
2023-01-05 14:43:23 +0100

    Feat - Fix country field in form

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -5,6 +5,32 @@ class Bottle < ApplicationRecord
 
   belongs_to :user
 
+  # COUNTRIES = %i[
+  #   Italie
+  #   France
+  #   Espagne
+  #   USA
+  #   Argentine
+  #   Australie
+  #   Chili
+  #   Afrique du Sud
+  #   Chine
+  #   Allemagne
+  #   Portugal
+  #   Russie
+  #   Roumanie
+  #   Hongrie
+  #   Nouvelle-Zélande
+  #   Brésil
+  #   Autriche
+  #   Grèce
+  #   Géorgie
+  #   Suisse
+  # ].freeze
+  COUNTRIES = %i[
+    France
+  ].freeze
+
   REGIONS = %i[
     Alsace-Lorraine
     Beaujolais
@@ -24,6 +50,7 @@ class Bottle < ApplicationRecord
 
   enumerize :region, in: REGIONS, predicates: true
   enumerize :color, in: COLORS, predicates: true
+  enumerize :country, in: COUNTRIES, predicates: true
 
   # Associations
   has_one_attached :photo
