51051dbe0b5ce5b0389966a47f21f6c398e1ba99
Francois Dumas <dumaslattaque.francois@gmail.com>
2023-01-20 21:58:08 +0100

    Feat - Add label in maintenance

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module RoundedImageHelper
+module ImageHelper
   def rounded_image(target_image, size = 40, classes = nil)
     if target_image.attached?
       image_tag(target_image.url,
@@ -14,6 +14,22 @@ module RoundedImageHelper
                 height: size,
                 width: size,
                 class: "inline-block rounded-full #{classes}")
+    end
+  end
+
+  def regular_image(target_image, size = 40, classes = nil)
+    if target_image.attached?
+      image_tag(target_image.url,
+                height: size,
+                width: size,
+                class: "inline-block h-auto #{classes}",
+                alt: "profile")
+    else
+      image_tag("kitten.jpg",
+                alt: "Default project image",
+                height: size,
+                width: size,
+                class: "inline-block h-auto #{classes}")
     end
   end
 end
